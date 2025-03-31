cf9bd095fdb85ac94b1658e01ef9e7896ceca8a1
SillyHippy <159052837+SillyHippy@users.noreply.github.com>
2025-03-31 10:06:50 -0500

    poiujhgfghjkl

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
+    }
   },
   build: {
     outDir: 'dist',
-  },
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  }
 });
